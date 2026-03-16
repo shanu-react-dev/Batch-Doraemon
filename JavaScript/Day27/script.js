8a68be4facbe41d11f3792173e99f4380735f204
shanu-react-dev <amarjeet.kr.dev@gmail.com>
2026-03-16 15:53:35 +0530

    variables and datattypes

---
+++ JavaScript/Day27/script.js
@@ -0,0 +1,1 @@
+console.log("Hello I am in the world of Web Dev");
---
+++ JavaScript/Day28/script.js
@@ -0,0 +1,50 @@
+// console.log("Hii This is JavaScript!!");
+// document.write("Hii This is Shanu");
+//! how to declare a variable
+// var , let, const
+var userName = "sundari";
+// console.log(userName);
+
+//? Identifier rules...
+//! Identifiers can't start with a number
+//! Keywords are not allowed as an identifiers
+//! special characters are not allowed as an identifiers expcept $ and _
+//! white space is not allowed inside an identifiers
+//! Numeric value can be used either in between the identifiers or at last
+// var 1stUser = "Amar";
+// var for = "Hii"
+// console.log(for)
+// var $myName = "Shanu";
+// console.log($myName);
+// var _frndName = "Sundari";
+// console.log(_frndName);
+// var first name = "Shanu"
+// var last name = "Singh"
+// console.log(first name)
+// var name1 = "Shanu";
+// var name2 = "Amar";
+// console.log(name1);
+// console.log(name2);
+
+var userName = "Anirudh";
+console.log(userName);
+console.log(typeof userName + " Datatype");
+var age = 33;
+console.log(age);
+console.log(typeof age + " datatype");
+var isMarried = false;
+console.log(isMarried);
+console.log(typeof isMarried + " datatype");
+//todo Why the type of null is an object???
+var kids = null;
+console.log(kids);
+console.log(typeof kids + " datatype");
+var followers = undefined;
+console.log(followers);
+console.log(typeof followers + " datatype");
+var starsIntheSky = 1234567890123456787655n;
+console.log(starsIntheSky);
+console.log(typeof starsIntheSky + " datatype");
+var uniqueValue = Symbol;
+console.log(uniqueValue);
+console.log(typeof uniqueValue);
